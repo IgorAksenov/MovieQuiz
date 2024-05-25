7fc504e5b419e466cc83f8ab090cc020d3983e09
Igor <aksenov.ivd@gmail.com>
2024-05-25 23:05:27 +0300

    Добавлен файл AlertPresenter, реализован паттерн делегат

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,32 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by  Игорь on 25.05.2024.
+//
+
+import UIKit
+
+class AlertPresenter {
+    private weak var viewController: UIViewController?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func show(alertModel: AlertModel) {
+        let alert = UIAlertController(
+            title: alertModel.title,
+            message: alertModel.message,
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(
+            title: alertModel.buttonText,
+            style: .default
+        ) { _ in
+            alertModel.completion?()
+        }
+        alert.addAction(action)
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+}
